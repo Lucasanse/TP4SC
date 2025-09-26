14c9bd2763fa1c341688c10ce07f33de13c18be9
Lucas San Segundo <89613102+Lucasanse@users.noreply.github.com>
2025-09-26 11:52:50 -0300

    sin comentario

--- Vista/ejercicio6/accionNuevaPersona.php
+++ Vista/ejercicio6/accionNuevaPersona.php
@@ -3,7 +3,7 @@ include_once '../../Control/ABMPersona.php';
 include_once '../../util/funciones.php';
 include_once '../estructura/header.php';
 
-$datos = data_submitted(); // tu función de util/funciones.php
+$datos = data_submitted(); 
 $objAbmPersona = new AbmPersona();
 
 $resp = $objAbmPersona->alta($datos);

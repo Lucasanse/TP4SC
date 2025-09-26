29ec8609f87bb9898b4ac37f389336aea9b3ac05
Lucas San Segundo <89613102+Lucasanse@users.noreply.github.com>
2025-09-26 11:39:07 -0300

    update persona

--- Vista/ejercicio5/autosPersona.php
+++ Vista/ejercicio5/autosPersona.php
@@ -7,28 +7,18 @@ include_once '../estructura/header.php';
     <form id="formAutosPersona" method="get" action="autosPersonaAction.php" class="needs-validation" novalidate>
         <div class="mb-3">
             <label for="dni" class="form-label">DNI de la persona</label>
-            <input type="number" class="form-control" id="dni" name="dni" required>
+            <input type="number" id="dni" name="dni" class="form-control dni" 
+                   placeholder="Ingrese DNI" required min="1000000" max="999999999">
             <div class="invalid-feedback">
                 Ingrese un DNI válido.
             </div>
+            
         </div>
         <button type="submit" class="btn btn-primary">Ver Autos</button>
         <a href="listaPersonas.php" class="btn btn-secondary">Ver lista de personas</a>
     </form>
 </div>
 
-<script>
-(() => {
-  'use strict';
-  const form = document.getElementById('formAutosPersona');
-  form.addEventListener('submit', event => {
-    if (!form.checkValidity()) {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-    form.classList.add('was-validated');
-  }, false);
-})();
-</script>
+<script src="../js/validarPersona.js"></script>
 
 <?php include_once '../estructura/footer.php'; ?>
